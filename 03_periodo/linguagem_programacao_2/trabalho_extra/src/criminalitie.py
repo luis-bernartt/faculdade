c04dd69fd90a506dfc2b4046c06196a6a40d5ec7
luis-bernartt <luis_bernartt@icloud.com>
2025-04-07 00:25:56 -0300

    modularizado criminalitie.py

--- 03_periodo/linguagem_programacao_2/trabalho_extra/src/criminalitie.py
+++ 03_periodo/linguagem_programacao_2/trabalho_extra/src/criminalitie.py
@@ -1,22 +1,24 @@
-import pandas as pd # type: ignore
+import pandas as pd  # type: ignore
 
-file_path =  "/home/luis-bernartt/faculdade/03_periodo/linguagem_programacao_2/trabalho_extra/repository/crimes_brasil_uf.csv"
+class CrimeMaisComum:
+    def __init__(self, file_path: str, estado: str):
+        self.file_path = file_path
+        self.estado = estado
+        self.dados = pd.read_csv(self.file_path, sep=',')
 
+    def crime_mais_frequente(self) -> str:
+        dados_estado = self.dados[self.dados['Uf'] == self.estado]
+        tipo_crime = pd.DataFrame(dados_estado['Tipo_crime']).drop_duplicates()
+        tipo_crime['quantidade'] = 0
 
-dados = pd.read_csv(file_path, sep=',')
+        count = 0
+        tipo_mais_comum = ''
 
-dados = dados[dados['Uf'] == 'Rio de Janeiro']
-
-tipo_crime = pd.DataFrame(dados['Tipo_crime']).drop_duplicates()
-tipo_crime['quantidade'] = 0
-count = 0
-
-for index, row in dados.iterrows():
-    for index2, tipo in tipo_crime.iterrows():
-        if row['Tipo_crime'] == tipo['Tipo_crime']:
-            tipo['quantidade'] += 1
-            if tipo['quantidade'] > count:
-                count = tipo['quantidade']
-                tipo_mais_comum = tipo['Tipo_crime']
-
-print(f'O crime mais comum é: {tipo_mais_comum}')
+        for _, row in dados_estado.iterrows():
+            for index2, tipo in tipo_crime.iterrows():
+                if row['Tipo_crime'] == tipo['Tipo_crime']:
+                    tipo_crime.at[index2, 'quantidade'] += 1
+                    if tipo_crime.at[index2, 'quantidade'] > count:
+                        count = tipo_crime.at[index2, 'quantidade']
+                        tipo_mais_comum = tipo['Tipo_crime']
+        return tipo_mais_comum
